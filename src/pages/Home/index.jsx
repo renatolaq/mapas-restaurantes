12cbfce283bcf1f0f0f41d9ff458c514f9bdf58b
Renato Pereira <renato.laq@gmail.com>
2021-07-21 13:39:33 -0300

    Implementação do Carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
+import Slider from 'react-slick';
 import logo from '../../assets/logo.svg';
+import restaurante from '../../assets/restaurante-fake.png';
+import { Card } from '../../components';
 // eslint-disable-next-line import/named
-import { Wrapper, Container, Search, Logo, Map } from './styles';
+import { Wrapper, Container, Carousel, Search, Logo, Map, CarouselTitle } from './styles';
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
-
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
   return (
     <Wrapper>
       <Container>
@@ -19,6 +31,16 @@ const Home = () => {
             trailingIcon={<MaterialIcon role="button" icon="search" />}>
             <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
           </TextField>
+          <CarouselTitle>Na sua área</CarouselTitle>
+          <Carousel {...settings}>
+            <Card photo={restaurante} />
+            <Card photo={restaurante} />
+            <Card photo={restaurante} />
+            <Card photo={restaurante} />
+            <Card photo={restaurante} />
+            <Card photo={restaurante} />
+            <Card photo={restaurante} />
+          </Carousel>
         </Search>
       </Container>
       <Map />
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-
+import Slider from 'react-slick';
 // Wrapper do Mapa
 
 export const Wrapper = styled.div`
@@ -33,4 +33,19 @@ export const Logo = styled.img`
 export const Map = styled.div`
   background-color: red;
   width: 500px;
+`;
+
+export const Carousel = styled(Slider)`
+  .slick.slide {
+    margin-right: 16px;
+  }
+`;
+
+export const CarouselTitle = styled.h1`
+  font-family: ${(props) => props.theme.fonts.regular};
+  color: ${(props) => props.theme.colors.text};
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 29px;
+  margin: 16px 0;
 `;
